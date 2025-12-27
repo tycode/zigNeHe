db16c0d36acc9698edf05938f690e2ad5066240e
Rob Rolls <rob@tycode.org>
2025-12-27 08:59:02 +0000

    build.zig: use b.createModule

--- Lesson02/build.zig
+++ Lesson02/build.zig
@@ -7,9 +7,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "Lesson02",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     switch (currentTarget.os.tag) {
